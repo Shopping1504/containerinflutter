77f20871fd064d30c8ccc1a96f01cc9f650d6ff7
VanLinh <linhnv@mioapp.vn>
2022-05-11 19:30:11 +0700

    design Home Page App

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,20 +1,39 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class  HomePage extends StatelessWidget {
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           children: [
-            const Text("Bluetooth mouse"),
-            TextField(
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            Padding(
+              padding: EdgeInsets.only(top: 80),
             ),
-            ElevatedButton(onPressed: null, child: Text("Check"))
+            const Text(
+              "Bluetooth mouse",
+              style: TextStyle(fontSize: 20, color: Colors.red),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 40),
+            ),
+            SizedBox(
+              width: 200,
+              child: TextField(
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 40),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Check"),
+            )
           ],
         ),
       ),
